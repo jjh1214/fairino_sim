c7e6432345fdeb93a51ce214a662c77f52e9f152
jjh1214 <scarlet777735@gmail.com>
2025-07-29 15:14:30 +0900

    Feat: Add robot_name launch args & adjust damping

--- launch/fairino_gz.launch.py
+++ launch/fairino_gz.launch.py
@@ -5,32 +5,46 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, OpaqueFunction, RegisterEventHandler
+from launch.actions import IncludeLaunchDescription, OpaqueFunction, RegisterEventHandler, DeclareLaunchArgument
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
 import shutil
 
 def generate_launch_description():
+
+    robot_name_arg = DeclareLaunchArgument(
+        "robot_name",
+        default_value="fairino5_v6",
+        description="Robot name for selecting the xacro model"
+    )
+
+    robot_name = LaunchConfiguration("robot_name")
 
     def robot_state_publisher(context):
         xacro_exe = shutil.which("xacro")
         if not xacro_exe:
             raise RuntimeError("❌ The xacro executable was not found. Please make sure it is installed by running: sudo apt install ros-jazzy-xacro")
 
+        select_robot = robot_name.perform(context)
         xacro_file = os.path.join(
             get_package_share_directory('fairino_sim'),
             'config',
             'frcobot_gz.urdf.xacro'
         )
 
-        robot_description_raw = os.popen(f'"{xacro_exe}" "{xacro_file}"').read()
+        robot_description_raw = os.popen(f'"{xacro_exe}" "{xacro_file}" robot_name:={select_robot}').read()
 
         package_path = get_package_share_directory('fairino_description')
         robot_description_raw = robot_description_raw.replace(
             'package://fairino_description',
-            f'file://{package_path}'
+            f'file://{package_path}',
+        )
+
+        robot_description_raw = robot_description_raw.replace(
+            'damping="0"',
+            f'damping="300"'
         )
 
         robot_description = {'robot_description': robot_description_raw}
@@ -57,7 +71,7 @@ def generate_launch_description():
         executable='create',
         output='screen',
         arguments=['-topic', 'robot_description', '-name',
-                   'fairino5_v6', '-allow_renaming', 'true'],
+                   robot_name, '-allow_renaming', 'true'],
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -111,6 +125,7 @@ def generate_launch_description():
             )
         ),        
 
+        robot_name_arg,
         ros_gz_bridge,
         gz_spawn_entity,
 
